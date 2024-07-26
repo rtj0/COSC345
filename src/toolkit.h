3f8cfcb35db0da8373bb0cdf50ff7d0550c36349
temch584 <temch584@student.otago.ac.nz>
2024-07-26 14:58:44 +1200

    Added intial Printing header file

---
+++ src/toolkit.h
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <fstream>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#include <termios.h>
+#endif
+
+// Function to mimic a delay
+void delay(int milliseconds)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    while (std::chrono::high_resolution_clock::now() - start < std::chrono::milliseconds(milliseconds))
+    {
+        // Busy-waiting loop to create a delay
+    }
+}
+
+// Function to disable keyboard input
+void disableInput()
+{
+#ifdef _WIN32
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    SetConsoleMode(hStdin, 0);
+#else
+    struct termios t;
+    tcgetattr(STDIN_FILENO, &t);
+    t.c_lflag &= ~ICANON; // Disable canonical mode
+    t.c_lflag &= ~ECHO;   // Disable echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+#endif
+}
+
+// Function to enable keyboard input
+void enableInput()
+{
+#ifdef _WIN32
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    SetConsoleMode(hStdin, ENABLE_ECHO_INPUT | ENABLE_INSERT_MODE | ENABLE_LINE_INPUT | ENABLE_MOUSE_INPUT | ENABLE_PROCESSED_INPUT | ENABLE_QUICK_EDIT_MODE | ENABLE_WINDOW_INPUT | ENABLE_VIRTUAL_TERMINAL_INPUT);
+#else
+    struct termios t;
+    tcgetattr(STDIN_FILENO, &t);
+    t.c_lflag |= ICANON; // Enable canonical mode
+    t.c_lflag |= ECHO;   // Enable echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+
+    tcflush(STDIN_FILENO, TCIFLUSH);
+#endif
+}
+
+void typePrint(std::string content)
+{
+    std::string token;
+    std::stringstream ss(content);
+    std::vector<std::string> tokens;
+
+    while (getline(ss, token, '@'))
+    {
+        tokens.push_back(token);
+    }
+
+    disableInput();
+    for (std::string &text : tokens)
+    {
+        for (char c : text)
+        {
+            std::cout << c << std::flush;
+            delay(25);
+        }
+        enableInput();
+        while (getchar() != '\n')
+        {
+        }
+        disableInput();
+    }
+    enableInput();
+}
+
+std::string getFileContent(std::string fileName)
+{
+    std::ifstream file(fileName);
+    std::string str;
+    std::string file_contents;
+    while (std::getline(file, str))
+    {
+        file_contents += str;
+        file_contents.push_back('\n');
+    }
+    return file_contents;
+}
